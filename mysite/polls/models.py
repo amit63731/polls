58c306adef01f34ce5acf8871e140b0b88d64260
amit <amit95820363731@gmail.com>
2017-08-20 16:41:09 +0530

    changed file

--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django.utils.encoding import smart_unicode
+import datetime
+from django.utils import timezone
 
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
@@ -7,6 +9,9 @@ class Question(models.Model):
 
     def was_published_recently(self):
         return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+        was_published_recently.admin_order_field = 'pub_date'
+        was_published_recently.boolean = True
+        was_published_recently.short_description = 'Published recently?'
 
     def __unicode__(self):
 		return smart_unicode(self.question_text)
--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Question
+from django.shortcuts import render
+from django.http import Http404
 
-# Create your views here.
+
+
+def index(request):
+	latest_question_list = Question.objects.order_by('pub_date')
+	output = [p.question_text for p in latest_question_list]
+	template  = "polls/index.html"
+	context = {'latest_question_list' : latest_question_list}
+	return render(request, template, context)
+
+def detail(request, question_id):
+    try:
+        question = Question.objects.get(pk=question_id)
+    except Question.DoesNotExist:
+        raise Http404("Question does not exist")
+    return render(request, 'polls/detail.html', {'question': question})
